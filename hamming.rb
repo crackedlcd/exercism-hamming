30ff084a8c589f8571d338003cd173f1e783d9bf
crackedlcd <vtorres739@gmail.com>
2015-03-23 11:56:41 -0400

    Refactor code.

--- hamming.rb
+++ hamming.rb
@@ -1,12 +1,9 @@
 class Hamming
+
   def self.compute(base, compare)
     raise ArgumentError if base.length != compare.length
-    diff = 0
-    (0..(base.length - 1)).each do |index|
-      if base[index] != compare[index]
-        diff += 1
-      end
-    end
-    diff
+    
+    (0..(base.length-1)).reduce(0) { |diff, index| base[index] != compare[index] ? diff + 1: diff }
   end
+
 end
